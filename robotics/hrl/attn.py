866d3761aa1886cf2b838e811cda74e80c3d99b4
Stalin <Mr.Young4Hot@gmail.com>
2022-07-26 17:48:51 +0800

    Finish: 1. AttnEnv Debugging TODO:

--- robotics/hrl/attn.py
+++ robotics/hrl/attn.py
@@ -20,12 +20,12 @@ def distance(pos_a, pos_b):
 class AttnEnv(fetch_env.FetchEnv, utils.EzPickle):
     def __init__(self, reward_type="dense"):
         self.max_horizon_dist = 0.35
-        self.max_horizon_count = 16
-        self.pad_obs = np.array([0] * 6)
+        self.max_horizon_count = 8
 
-        self.self_feature_shape = None
-        self.sing_obstacle_feature_shape = 6
-        self.obstacle_feature_shape = self.sing_obstacle_feature_shape * self.max_horizon_count
+        self.sing_obstacle_feature_size = 18
+        self.self_feature_size = None
+        self.obstacle_feature_size = self.sing_obstacle_feature_size * self.max_horizon_count
+        self.pad_obs = np.array([0] * self.sing_obstacle_feature_size)
 
         initial_qpos = {
             "robot0:slide0": 0.405,
@@ -53,55 +53,33 @@ class AttnEnv(fetch_env.FetchEnv, utils.EzPickle):
         )
         utils.EzPickle.__init__(self, reward_type=reward_type)
 
-    def append_physical_feature(self, name) -> list:
+    def append_physical_feature(self, name) -> np.ndarray:
         assert isinstance(name, str)
         dt = self.sim.nsubsteps * self.sim.model.opt.timestep
         grip_pos = self.sim.data.get_site_xpos("robot0:grip").copy()
         grip_velp = self.sim.data.get_site_xvelp("robot0:grip").copy() * dt
 
-        # positions
+        # size
+        geom_id = self.sim.model.geom_name2id(name)
+        size = self.sim.model.geom_size[geom_id]
+        # position
         site_pos = self.sim.data.get_site_xpos(name).copy()
-        # rotations
+        # rotation
         rot = rotations.mat2euler(self.sim.data.get_site_xmat(name).copy())
-        # velocities
+        # velocity
         velp = self.sim.data.get_site_xvelp(self.achieved_name).copy() * dt
         velr = self.sim.data.get_site_xvelr(self.achieved_name).copy() * dt
         # state
         rel_pos = site_pos - grip_pos
         velp -= grip_velp
 
-        if name != self.achieved_name:
-            return [site_pos, rot, velp, velr, rel_pos]
-        else:
-            return [site_pos, rot, velp, velr, rel_pos]
+        features = np.concatenate([size, site_pos, rot, velp, velr, rel_pos])
+        assert features.size == self.sing_obstacle_feature_size
+        return features
 
 
     def _get_obs(self):
         dt = self.sim.nsubsteps * self.sim.model.opt.timestep
-        grip_pos = self.sim.data.get_site_xpos("robot0:grip").copy()
-        grip_velp = self.sim.data.get_site_xvelp("robot0:grip").copy() * dt
-        robot_qpos, robot_qvel = robot_utils.robot_get_obs(self.sim)
-
-        gripper_state = robot_qpos[-2:]
-        gripper_vel = (
-                robot_qvel[-2:] * dt
-        )
-        gripper_obs = [grip_pos, gripper_state, grip_velp, gripper_vel]
-
-        obs = gripper_obs.copy()
-        origin_point = self.cube_starting_point.copy()
-        obs.append(self.append_physical_feature(self.achieved_name))
-
-        count = self.max_horizon_count
-        for obstacle_name in self.obstacle_name_list:
-            obstacle_pos = self.sim.data.get_geom_xpos(obstacle_name).copy()
-            if distance(origin_point, obstacle_pos) < self.max_horizon_dist:
-                obs.append(self.append_physical_feature(obstacle_name))
-                count -= 1
-            if count == 0:
-                break
-        for _ in range(count):
-            obs.append(self.pad_obs)
 
         achieved_goal = self.sim.data.get_geom_xpos(self.achieved_name).copy()
 
@@ -110,8 +88,32 @@ class AttnEnv(fetch_env.FetchEnv, utils.EzPickle):
         else:
             goal = self.removal_goal.copy()
 
+        grip_pos = self.sim.data.get_site_xpos("robot0:grip").copy()
+        grip_velp = self.sim.data.get_site_xvelp("robot0:grip").copy() * dt
+        robot_qpos, robot_qvel = robot_utils.robot_get_obs(self.sim)
+        gripper_state = robot_qpos[-2:]
+        gripper_vel = (
+                robot_qvel[-2:] * dt
+        )
+        gripper_obs = [grip_pos, gripper_state, grip_velp, gripper_vel]
+        self.self_feature_size = np.concatenate(gripper_obs).size + achieved_goal.size + goal.size
+
+        obs = gripper_obs.copy()
+        origin_point = self.cube_starting_point.copy()
+        count = self.max_horizon_count
+        for object_name in self.object_name_list:
+            obstacle_pos = self.sim.data.get_geom_xpos(object_name).copy()
+            if distance(origin_point, obstacle_pos) < self.max_horizon_dist:
+                obs.append(self.append_physical_feature(object_name))
+                count -= 1
+            if count == 0:
+                break
+        for _ in range(count):
+            obs.append(self.pad_obs)
+
+        assert self.self_feature_size is not None
         return {
-            "observation": np.concatenate(obs).copy(),
             "achieved_goal": achieved_goal.copy(),
             "desired_goal": goal.copy(),
+            "observation": np.concatenate(obs).copy(),
         }
