b7de7f9b6df0222249ded3a18171f2ebce44dfbe
Stalin <Mr.Young4Hot@gmail.com>
2022-08-17 18:25:48 +0800

    Finish: 1. do not return value TODO:

--- robotics/render_env.py
+++ robotics/render_env.py
@@ -50,7 +50,7 @@ class RenderEnv(gym.Env):
 
     def step(self, action: np.ndarray):
         obs = self.model.get_obs()
-        prev_success_rate, value = self.agent.policy.predict_observation(obs)
+        prev_success_rate = self.agent.policy.predict_observation(obs)
         # print(f'Previous success rate: {prev_success_rate}')
 
         if action is None:
@@ -73,7 +73,7 @@ class RenderEnv(gym.Env):
             else:
                 self.model.macro_step_setup(planning_action)
         obs, _, done, info = self.macro_step(obs)
-        curr_success_rate, value = self.agent.policy.predict_observation(self.model.get_obs())
+        curr_success_rate = self.agent.policy.predict_observation(self.model.get_obs())
         # print(f'Current success rate: {curr_success_rate}')
 
         return obs, curr_success_rate - prev_success_rate, done, info
